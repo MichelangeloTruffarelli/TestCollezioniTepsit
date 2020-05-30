a664f9c1fa636f53aa93d784abc24ed71c71ba93
Michelangelo <Michelangelo@DESKTOP-C01FU0U>
2020-05-30 14:31:02 +0200

    Add project files.

---
+++ Collezioni/Program.cs
@@ -0,0 +1,24 @@
+﻿using Metodi;
+using System;
+
+namespace Collezioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci la prima stringa: ");
+            string s1 = Console.ReadLine();
+            Console.Write("Inserisci la seconda stringa: ");
+            string s2 = Console.ReadLine();
+            bool uguaglianza = MetodiCollezioni.StringheUguali(s1, s2);
+            Console.Write("Le stringhe sono uguali? " + uguaglianza);
+            Console.WriteLine();
+            Console.Write("Inserisci una stringa: ");
+            string s3 = Console.ReadLine();
+            bool maiuscole = MetodiCollezioni.ContainsDoubleChar(s3);
+            Console.Write("La stringa contiene lettere maiuscole? " + maiuscole);
+
+        }
+    }
+}
---
+++ Metodi/MetodiCollezioni.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Metodi
+{
+    public class MetodiCollezioni
+    {
+        public static bool StringheUguali(string s1, string s2)
+        {
+            if (s1 == s2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool ContainsDoubleChar(string s3)
+        {
+            int maiuscole = 0;
+            for (int i = 0; i < s3.Length; i++)
+            {
+                if (Char.IsUpper(s3[i]) == true)
+                    maiuscole++;
+            }
+            if (maiuscole > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
